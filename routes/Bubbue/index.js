1ae70a4d6fdbb24e5bc1a92892b8d5c2c3bb23cb
Chibuokem Onyekwelu <chibuokem_tolu@hotmail.com>
2019-04-23 09:53:01 +0100

    Bubbue Completed

---
+++ routes/Bubbue/index.js
@@ -0,0 +1,109 @@
+const express = require('express')
+const mongodb = require('mongodb')
+const router = express.Router()
+
+const Bubbue = require('../../models/Bubbue')
+
+router.get('/', (req, res) => {
+	Bubbue.find((err, data) => {
+		if (err) res.send({
+			data: null,
+			message: err,
+			error: true
+		})
+
+		res.send({
+			data,
+			message: 'Bubbue Setup fetched successfully',
+			error: false
+		})
+	})
+})
+
+router.get('/:area', (req, res) => {
+	const { area } = req.params
+
+	Bubbue.findOne({ area }, (err, data) => {
+		if (err) res.send({
+			data: null,
+			message: err,
+			error: true
+		})
+
+		res.send({
+			data,
+			message: 'Bubbue setup fetched successfully',
+			error: false
+		})
+	})
+})
+
+router.put('/:area', (req, res) => {
+	const { area } = req.params
+
+	Bubbue.findByIdAndUpdate({ area }, { ...req.body }, (err, data) => {
+		if (err) res.send({
+			data: null,
+			message: err,
+			error: true
+		})
+
+		res.send({
+			data,
+			message: 'Updated',
+			error: false
+		})
+	})
+})
+
+router.route('/:area').delete((req, res) => {
+	const { area } = req.params
+
+	Bubbue.findByIdAndUpdate({ area }, { isDeleted: true }, (err, data) => {
+		if (err) res.send({
+			data: null,
+			message: err,
+			error: true
+		})
+
+		res.send({
+			data,
+			message: 'Deleted',
+			error: false
+		})
+	})
+})
+
+router.post('/add', (req, res) => {
+	const { area } = req.body
+
+	Bubbue.findOne({ area, isDeleted: false }, (err, data) => {
+		if (err) res.send({
+			data: null,
+			message: err,
+			error: true
+		})
+
+		if (data !== null) res.send({
+			data: null,
+			message: 'exists',
+			error: true
+		})
+		else {
+			new Bubbue({ ...req.body })
+				.save()
+				.then(data => res.send({
+					data,
+					message: 'Added Successfully',
+					error: false
+				}))
+				.catch(error => res.send({
+					data: null,
+					message: error,
+					error: true
+				}))
+		}
+	})
+})
+
+module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,11 @@ const express = require('express')
 const app = express()
 
 const Auth = require('./User/')
+const Bubbue = require('./Bubbue/')
 // const Orders = require('./Order/')
 
 app.use('/user', Auth)
+app.use('/bubbue', Bubbue)
 // app.use('/orders', Orders)
 
 module.exports = app
